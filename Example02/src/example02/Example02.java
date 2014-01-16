da0882432022d05de2de153165fd9685479c3605
Wes Gilleland <wes.gilleland@gmail.com>
2014-01-16 10:26:16 -0500

    1/16/14 - Checkpoint 2

--- Example02/src/example02/Example02.java
+++ Example02/src/example02/Example02.java
@@ -10,13 +10,43 @@ package example02;
  *
  * @author Deathnerd
  */
+
+import java.util.Scanner;
 public class Example02 {
 
 	/**
 	 * @param args the command line arguments
 	 */
 	public static void main(String[] args) {
-		// TODO code application logic here
+		
+		Scanner input = new Scanner(System.in);
+		
+		System.out.println("Enter two end points of a diagonal in 1st rectangle: ");
+		float x1 = input.nextFloat();
+		float y1 = input.nextFloat();
+		float x2 = input.nextFloat();
+		float y2 = input.nextFloat();
+		
+		Point pt1 = new Point(x1, y1);
+		Point pt2 = new Point(x2, y2);
+		
+		Rectangle r1 = new Rectangle(pt1, pt2);
+		
+		System.out.println("Enter two end points of a diagonal in 1st rectangle: ");
+		x1 = input.nextFloat();
+		y1 = input.nextFloat();
+		x2 = input.nextFloat();
+		y2 = input.nextFloat();
+		
+		pt1 = new Point(x1, y1);
+		pt2 = new Point(x2, y2);
+		
+		Rectangle r2 = new Rectangle(pt1, pt2);
+		
+		r1.display();
+		r2.display();
+		
+		r1.overlap(r2); //check if r1 overlaps r2
 	}
 	
 }
@@ -74,9 +104,18 @@ class Rectangle{
 	}
 	
 	void overlap(Rectangle another){
-		if(another.tr.y < bl.y || another.bl.y > tr.y ||
-				another.tr.x < bl.x || another.bl.x > tr.x){ //rectangles are disjointed (not overlapping or contained)
+		if(another.tr.y < bl.y || another.bl.y > tr.y ||	//rectangles are disjointed (not overlapping or contained)
+			another.tr.x < bl.x || another.bl.x > tr.x){
 			System.out.println("The rectangles are disjointed");
+		}
+		else if(bl.x <= another.bl.x && tr.x >= another.tr.x &&	 //if one rectangle is contained within one another
+				bl.y <= another.bl.y && tr.y >= another.tr.y &&
+				another.bl.x <= bl.x && another.tr.x >= tr.x &&
+				another.bl.y <= bl.y && another.tr.y >= tr.y){
+			System.out.println("Rectangles are contained");
+		}
+		else{
+			System.out.println("The rectangles overlap");
 		}
 	}
 }
