048fdea6f4c7d0f8c541d89dc8af6a95eee06f1b
Wes Gilleland <wes.gilleland@gmail.com>
2014-01-22 12:21:03 -0500

    end of 1/22/14

---
+++ lab2/src/lab2/Lab2.java
@@ -0,0 +1,103 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+
+package lab2;
+
+import java.util.Scanner;
+
+/**
+ * Course: CSC191, Spring 2014
+ * Projet: Lab1
+ * Date: 1/22/2014
+ * Author: Wes Gilleland
+ * Purpose: This program reads the height of a triangle (less than 10), draws it,
+ * and rotates it 90 degrees, 180 degrees, and 270 degrees clockwise
+ */
+public class Lab2 {
+
+	static class NumberTriangle{
+		private int height; // height of the triangle (assume it’s always less than 14) 
+		private int number = 1; //the number we're manipulating
+
+		// Set the height with h  
+		void setHeight(int h){
+			height = h; 
+		}
+
+		// Draw the triangle 
+		public void printTriangle(){
+			if(height <= 0) //check for no height
+				return;
+			
+			int count = 1;
+			int multiplier = 0;
+			
+			while(count <= height){
+				System.out.print(number+" ");
+			}
+			
+		}
+
+		// Rotate the triangle 90 degrees clockwise and draw it 
+		public void rotateTriangle90(){
+			if(height <= 0) //check for no height
+				return;
+			
+			double num = Math.pow(2, height-1);
+			int row = 1;
+			double temp = num;
+			
+			System.out.println("Num = "+num);
+			
+//			while(row <= (height*2-1)){ //couunt through all the rows
+//				
+//				row++;
+//			}
+			
+			while(row <= height){ //print half rows up to the 1's row
+				for(int i = 1; i <= row; i++)
+					System.out.print(temp+" ");
+				System.out.println();
+				row++;
+				temp /= 2;
+			}
+			temp *= 2;
+			while(row <= (height*2-1)) { //print the other half
+				temp *= 2;
+				for(int i = 1; i<= (height*2-row); i++)
+					System.out.print(temp+ " ");
+				row++;
+				System.out.println();
+			}
+		}
+
+		// Rotate the triangle 180 degrees clockwise and draw it 
+		public void rotateTriangle180(){
+			if(height <= 0) //check for no height
+				return;
+		}
+
+		// Rotate the triangle 270 degrees clockwise and draw it 
+		public void rotateTriangle270(){
+			if(height <= 0) //check for no height
+				return;
+		}
+	}
+
+	public static void main(String[] args){
+		Scanner in = new Scanner(System.in);
+		
+		System.out.println(11/2);
+		
+		NumberTriangle triangle = new NumberTriangle();
+		
+		System.out.print("Enter a height: ");
+		int height = in.nextInt();
+		
+		triangle.setHeight(height);
+		triangle.rotateTriangle90();
+	}
+}
