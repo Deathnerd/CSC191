4b5335137292f696f1d48f73ed533cdd22aeeffd
unknown <wes.gilleland@gmail.com>
2014-02-01 00:29:48 -0800

    Gotta fix the index array bounds bug

--- Lab3/src/lab3.java
+++ Lab3/src/lab3.java
@@ -8,6 +8,8 @@
  */
 
 import java.util.Scanner;
+import java.util.Random;
+
 class PermutationGenerator 
 {
     int maxnum; // permutation is for numbers 1 ~ maxnum
@@ -17,15 +19,26 @@ class PermutationGenerator
     // generate a random permutation of numbers 1 ~ maxnum
     // return a permutation array which stores a random permutation
     int[] generate(){
+		Random rand = new Random();
         // array for a random permutation
         int permutation[] = new int[maxnum];
+		int size = maxnum;
         
         // numbers not yet in a permutation
-        int second[] = new int[maxnum]; 
-        for (int i = 0; i < maxnum; i++){
-            second[i] = i+1;
-        }
+        int A[] = new int[maxnum];
+		for(int i = 0; i < 10; i++){ //create the array we're gonna randomly pick from
+			A[i] = i+1;
+		}
+
         //make permutation array
+		
+		for(int i = 0; i <10; i++){
+			int pos = rand.nextInt(size);
+			System.out.println(pos);
+			permutation[i] = A[pos];
+			MyListOperations.removeListElement(A, size, pos);
+			size--;
+		}
         return permutation;
     }
     
@@ -40,7 +53,17 @@ class MyListOperations{
     // remove A[pos] from A[0]~A[size-1]
     static void removeListElement(int A[], int size, int pos){
     // your code comes here
-		//remove by making a new empty array with 
+		pos = pos-1;
+		System.out.println(pos);
+		//shift the A[pos] to the right currentSize-1
+		int temp = A[pos];
+		//to the swapping!
+		int i = pos;
+		while(i < size){
+			A[i]=A[i+1];
+			i++;
+		}
+		A[size]=temp;
     }
     // print A[0]~A[n-1]
     static void printList(int A[], int size){
@@ -55,7 +78,7 @@ public class lab3{
     public static void main(String[] args){
         Scanner in = new Scanner(System.in);
         PermutationGenerator pg = new PermutationGenerator(10);
-        
-        pg.generate();
+		
+		pg.run();
     }
 }
