0181f59308a211f077e26831fbcebef3d1d9722e
Wes Gilleland <wes.gilleland@gmail.com>
2014-02-11 08:15:36 -0500

    start

---
+++ Homework3/src/homework3/Homework3.java
@@ -0,0 +1,42 @@
+package homework3;
+
+class TicTacToe {
+
+	private int size; // should be 3 
+	private char S[][]; // a size*size array to represent the grid 
+
+	final static char user = 'U'; // used by user to mark a cell 
+	final static char computer = 'C';//used by computer to mark a cell 
+
+	// constructor 
+	TicTacToe() {
+		// your come comes here
+		size = 3;
+	}
+
+	// print the current content of the grid 
+	void print() {
+		// your come comes here 
+	}
+
+	// your code comes here to implement other necessary methods 
+	void placeToken(int x, int y) {
+
+	}
+
+	boolean checkWin() {
+		return false;
+	}
+
+}
+
+public class Homework3 {
+
+	/**
+	 * @param args the command line arguments
+	 */
+	public static void main(String[] args) {
+		// TODO code application logic here
+	}
+
+}
