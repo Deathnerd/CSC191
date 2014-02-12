3f22d13616248320f9454ecd4b4ea3e02f6b2502
Wes Gilleland <wes.gilleland@gmail.com>
2014-02-12 15:19:45 -0500

    Finished

--- Lab05/src/lab05/Lab05.java
+++ Lab05/src/lab05/Lab05.java
@@ -10,29 +10,32 @@ import java.util.Scanner;
 
 class DM{
 	static int divide(int numerator, int denominator){
-		if(denominator > numerator)
+		if(denominator > numerator) //if we have a decimal fraction
 			return 0;
-		else if((numerator - denominator) < denominator)
+		else if((numerator - denominator) < denominator) //base case
 			return 1;
-		else
+		else //keep going
 			numerator -= denominator;
 		
 		return 1+divide(numerator, denominator);
 	}
 	
 	static int mod(int numerator, int denominator){
-		if(numerator < denominator)
+		if(numerator < denominator) //base case
 			return numerator;
-		else
+		else{
 			numerator -= denominator;
-		
-		return mod(numerator, denominator);
+			return mod(numerator, denominator);
+		}
 	}
 }
 public class Lab05 {
 	public static void main(String[] args) {
-		System.out.println(6/7);
-		System.out.println(DM.divide(5,1));
-		System.out.println(DM.mod(5, 2));
+		Scanner in = new Scanner(System.in);
+		System.out.print("Enter two positive integers: ");
+		int numerator = in.nextInt();
+		int denominator = in.nextInt();
+		System.out.println(numerator+" / "+denominator+" = "+DM.divide(numerator, denominator));
+		System.out.println(numerator+" % "+denominator+" = "+DM.mod(numerator, denominator));
 	}
 }
