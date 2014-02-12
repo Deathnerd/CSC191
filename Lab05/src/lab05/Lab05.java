6233db564fb298bf2a1562d4c5c026430a12def5
Wes Gilleland <wes.gilleland@gmail.com>
2014-02-12 09:28:18 -0500

    Beginning

---
+++ Lab05/src/lab05/Lab05.java
@@ -0,0 +1,13 @@
+/**
+ * Course: CSC191, Spring 2014 
+ * Project: Homework3 
+ * Date: 2/12/2014 
+ * Author: Wes Gilleland 
+ * Purpose: A recursive implementation of mod and divide
+ */
+package lab05;
+import java.util.Scanner;
+public class Lab05 {
+	public static void main(String[] args) {
+	}
+}
