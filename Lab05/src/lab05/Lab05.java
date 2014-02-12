2ef8d9d4fddb729d7f6000370c78a4db209ae78f
Wes Gilleland <wes.gilleland@gmail.com>
2014-02-12 10:22:40 -0500

    division working, need mod

--- Lab05/src/lab05/Lab05.java
+++ Lab05/src/lab05/Lab05.java
@@ -7,7 +7,32 @@
  */
 package lab05;
 import java.util.Scanner;
+
+class DM{
+	static int divide(int numerator, int denominator){
+		if(denominator > numerator)
+			return 0;
+		else if((numerator - denominator) < denominator)
+			return 1;
+		else
+			numerator -= denominator;
+		
+		return 1+divide(numerator, denominator);
+	}
+	
+	static int mod(int numerator, int denominator){
+		if(numerator < denominator)
+			return numerator;
+		else
+			numerator -= denominator;
+		
+		return mod(numerator, denominator);
+	}
+}
 public class Lab05 {
 	public static void main(String[] args) {
+		System.out.println(6/7);
+		System.out.println(DM.divide(5,1));
+		System.out.println(DM.mod(5, 2));
 	}
 }
