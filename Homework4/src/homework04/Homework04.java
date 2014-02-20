12275b24602ae6f088f39a51c63a85398473c6ae
Wes Gilleland <wes.gilleland@gmail.com>
2014-02-20 00:45:13 -0500

    RLC not running correctly, but it runs

--- Homework4/src/homework04/Homework04.java
+++ Homework4/src/homework04/Homework04.java
@@ -7,11 +7,13 @@
 // Purpose: This program will create an N*N grid and walk it in a zig-zag
 // pattern while compressing the sequence using Run Length Coding
 // ------------------------------
-
 package homework04;
+
 import java.util.Scanner;
+import java.util.Random;
 
 class Walk {
+
 	private int maxSize;
 	private int A[][];
 
@@ -40,144 +42,129 @@ class Walk {
 	// Construct a maxSize by maxSize two dimensional array A and 
 	// randomly set elements with numbers 0 and 1 
 	Walk(int ms) {
-		// your code 
-		rlc = new int [ms+1][2];
-		A = new int [ms][ms];
+		System.out.println(ms*ms);
+		rlc = new int[ms * ms][2];
+		A = new int[ms][ms];
 		maxSize = ms;
+		Random rand = new Random();
+
+		for (int i = 0; i < maxSize; i++)
+			for (int j = 0; j < maxSize; j++)
+				A[i][j] = rand.nextInt(2);
 		
-		int c = 1;
-		for(int i = 0; i < maxSize; i++){
-			for(int j = 0; j < maxSize; j++){
-				A[i][j] = c;
-				c++;
-			}
+		//set rlc
+		//set the first value of rlc
+		for(int i = 0; i <= ms*ms-1; i++){
+			rlc[i][0] = -1;
+			rlc[i][1] = 0;
 		}
-	} 
- 
-  
+	}
+
 	// print the content of the grid
 	void print() {
 		System.out.print("Grid:\n======");
 		System.out.println();
-		for(int i = 0; i < maxSize; i++){
-			for(int j = 0; j < maxSize; j++)
-				System.out.print(A[i][j]+" ");
+		for (int i = 0; i < maxSize; i++) {
+			for (int j = 0; j < maxSize; j++) {
+				System.out.print(A[i][j] + " ");
+			}
 			System.out.println();
 		}
 	}
 
 	// zigzag scan the grid and do run-length coding 
 	void runLengthCoding() {
-		// your code 
-	}
-	
-	// print the run-length coding result, i.e., content of rlc[][] 
-	void printCodingResult() {
-		// your code 
-	}
-	
-	void zigzagWalk1() {
-		int flag = 0; //0: up 	1: down
-		for (int i = 1; i <= maxSize * 2 - 1; i++) { //maxSize*2-1 segments in total
-			int r, c; //A[r][c] is the cell that is currently accessed
-
-			//inititalize r and c for the starting point of a segment
-			if (flag == 0) { //if we're moving up
-				if (i < maxSize) {
-					c = 0;
-					r = i - 1;
-				} else {
-					r = maxSize - 1;
-					c = i - maxSize;
-				}
-			} else { //if we're moving down
-				if (i <= maxSize) {
-					r = 0;
-					c = i - 1;
-				} else {
-					c = maxSize - 1;
-					r = i - maxSize;
-				}
-			}
-			//traverse every cell in a certain segment
-			while (r >= 0 && c >= 0 && r <= maxSize - 1 && c <= maxSize - 1) {
-				System.out.print(A[r][c] + " ");
-
-				if (flag == 0) { //move to the right one and up one
-					r--; //move up
-					c++; //move right
-				} else { //move to the left one and down one
-					r++; //move down
-					c--; //move left
-				}
-			}
-			System.out.println();
-			flag = (flag + 1) % 2;
-		}
-	}
-	
-	//all examples for a 3x3 grid
-	void zigzagWalk2(){
+		
+		int last = -1;
+		int count = 0;
+		
 		int flag = 0; //0 is down, 1 is up
 		int m = 0;
-		for(int i = 1; i <= maxSize*2-1; i++){ //2maxSize-1 segments
+		for (int i = 1; i <= maxSize * 2 - 1; i++) { //2maxSize-1 segments
 			int r, c; //start at the top right corner
 			//if moving down and right
-			if(flag == 0){
+			if (flag == 0) {
 				if (i < maxSize) {
-					c = maxSize-i;
+					c = maxSize - i;
 					r = 0;
-				} else {	
+				} else {
 					c = 0;
 					r = i - maxSize;
 				}
 			} else { //if moving up and left
 				if (i <= maxSize) { //repeats twice
 					c = maxSize - 1;
-					r = i-1;
+					r = i - 1;
 				} else {
-					r = maxSize-1;
+					r = maxSize - 1;
 					m += 2;
-					if(maxSize % 2 == 0)
-						c = maxSize-flag-m;
-					else
-						c = maxSize - m; //this one
-//					System.out.println("C = "+c);
+					if (maxSize % 2 == 0) { //dirty hack, but it works
+						c = maxSize - flag - m;
+					} else {
+						c = maxSize - m;
+					}
 				}
 			}
-			
 			//traverse the segment
 			//r >= 0 && c >= 0 keeps us above the lower bound of the array
 			//r <= maxSize-1 && c <= maxSize-1 keeps us below the upper bound
-			while(r >= 0 && c >= 0 && r <= maxSize - 1 && c <= maxSize - 1){
-				System.out.print(A[r][c]+ " ");
-				if(flag == 0){ //moving down
+			while (r >= 0 && c >= 0 && r <= maxSize - 1 && c <= maxSize - 1) {
+				if(last == -1){ //if this is the first run
+					count++; //count the first one
+//					last = A[r][c];
+				}
+				else if (last == A[r][c]){ //if the last element matches the current one
+					count++;
+				} else { //the last element doesn't match the current one
+					//record the number of repetitions
+					rlc[idx][0] = last;
+					rlc[idx][1] = count;
+					count = 0; //reset the counter
+					idx++; //increment the index
+				}
+				System.out.print(A[r][c] + " ");
+				last = A[r][c];
+				if (flag == 0) { //moving down
 					r++; //move to the right
 					c++; //move down
-				} else{ //moving up
+				} else { //moving up
 					r--; //move left
 					c--; //move up
 				}
 			}
+			
 			System.out.println();
 			flag = (flag + 1) % 2;
 		}
 	}
+
+	// print the run-length coding result, i.e., content of rlc[][] 
+	void printCodingResult() {
+		System.out.printf("1(%d,%d)\n", rlc[1][0], rlc[1][1]);
+		System.out.printf("2(%d,%d)\n", rlc[2][0], rlc[2][1]);
+		System.out.printf("3(%d,%d)\n", rlc[3][0], rlc[3][1]);
+		System.out.printf("4(%d,%d)\n", rlc[4][0], rlc[4][1]);
+		System.out.printf("5(%d,%d)\n", rlc[5][0], rlc[5][1]);
+		System.out.printf("6(%d,%d)\n", rlc[6][0], rlc[6][1]);
+		int i = 1;
+//		while(rlc[i][0] != -1){
+//			System.out.printf("(%d,%d)\n", rlc[i][0], rlc[i][1]);
+//			i++;
+//		}
+	}
 }
+
 public class Homework04 {
 
 	public static void main(String[] args) {
 		Scanner in = new Scanner(System.in);
-		
+
 		System.out.print("Enter n (positive integer): ");
 		Walk walk = new Walk(in.nextInt());
-		
+
 		walk.print();
 		System.out.println("\n");
-		walk.zigzagWalk1();
-		System.out.println();
-		walk.zigzagWalk2();
-		System.out.println();
-//		walk.zigzagWalk3();
+		walk.runLengthCoding();
+		walk.printCodingResult();
 	}
 }
