9577197f283ad8ac088ba587fbb4ac9631859878
Wes Gilleland <wes.gilleland@gmail.com>
2014-06-15 00:31:03 -0400

    final commit

--- Homework12/src/homework12/homework12.java
+++ Homework12/src/homework12/homework12.java
@@ -1,6 +1,7 @@
 package homework12;
 
 import javax.swing.*;
+import javax.swing.border.Border;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -42,12 +43,33 @@ class DrawPanel extends JPanel {
 		}
 	}
 
+	void sierpenski(Graphics graphics, int order, Point point1, Point point2, Point point3){
+		if(order < 0)
+			return;
+
+		//setup midpoints
+		Point midpointP12 = midpoint(point1, point2);
+		Point midpointP23 = midpoint(point2, point3);
+		Point midpointP31 = midpoint(point3, point1);
+
+		//recursive calls
+		sierpenski(graphics, --order, point1, midpointP12, midpointP31);
+		sierpenski(graphics, --order, midpointP12, point2 , midpointP23);
+		sierpenski(graphics, --order, midpointP31, midpointP23, point3);
+	}
+
+	//one-liner to calculate and return a midpoint Point
+	Point midpoint(Point p1, Point p2){
+		return new Point((p1.x + p2.x) / 2,(p1.y + p2.y) / 2);
+	}
+
+	//draw concurrent circles
 	void drawCircles(int order, Graphics graphics, int size){
 		if(order < 0)
 			return;
 		int x = getWidth() - size;
 		int y = getHeight() - size;
-		graphics.drawOval(x, y, size, size);
+		graphics.drawOval(x, y, size*2, size*2);
 		drawCircles(--order, graphics, size+5);
 	}
 }
@@ -58,27 +80,63 @@ public class homework12  extends JFrame implements ActionListener{
 
 	JPanel orderPanel;
 	JTextField orderText;
+	JLabel label;
 
 	DrawPanel drawPanel;
 
 	int option;
 
 	homework12(){
+		option = 0;
 		setLayout(new BorderLayout(0,0));
 
 		//make the shape buttons
 		shapeButtons = new JButton[2];
 		shapeButtons[0] = new JButton("Co-centric circles");
 		shapeButtons[1] = new JButton("Sierpenski Triangle");
-
-		//make the
+		//make the shape button panel
+		shapeButtonPanel = new JPanel();
+		shapeButtonPanel.setLayout(new GridLayout(1, shapeButtons.length));
+		add(shapeButtonPanel, BorderLayout.NORTH);
+		//add the buttons and set up their listeners
+		for(int i = 0; i < shapeButtons.length; i++){
+			shapeButtonPanel.add(shapeButtons[i]);
+			shapeButtons[i].addActionListener(this);
+		}
+		label = new JLabel("Enter an integer >= 0: ");
+		orderText = new JTextField("0", 5);
+		//make the order panel
+		orderPanel = new JPanel();
+		orderPanel.setLayout(new GridLayout(1,4));
+		add(orderPanel, BorderLayout.SOUTH);
+		orderPanel.add(label);
+		orderPanel.add(orderText);
+		orderText.addActionListener(this);
+		//add the draw panel to the center
+		drawPanel = new DrawPanel();
+		add(drawPanel, BorderLayout.CENTER);
 	}
 
 	public void actionPerformed(ActionEvent event){
+		//action logic goes here
+		for(int i = 0; i < shapeButtons.length; i++){
+			if(event.getSource() == shapeButtons[i]){
+				drawPanel.option = i;
+			}
+		}
+
+		if(event.getSource() == orderText){
+			drawPanel.num = Integer.parseInt(orderText.getText());
+		}
 
 	}
 
 	public static void main(String[] args){
+		homework12 app = new homework12();
 
+		app.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		app.setSize(500,500);
+		app.setVisible(true);
+		app.setTitle("Homework 12");
 	}
 }
